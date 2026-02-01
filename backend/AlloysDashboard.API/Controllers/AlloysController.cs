106d5d75d45eb0c8dae4b9b6170276fd04a3b693
arlealexandre <arle.alexandre@protonmail.com>
2026-02-01 09:51:30 +0100

    Added first controller

---
+++ backend/AlloysDashboard.API/Controllers/AlloysController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlloysDashboard.API.Controllers;
+
+[ApiController]
+[Route("api/alloys")]
+public class AlloysController : ControllerBase
+{
+    public AlloysController()
+    {
+        
+    }
+
+}
--- backend/AlloysDashboard.API/Program.cs
+++ backend/AlloysDashboard.API/Program.cs
@@ -1,15 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
